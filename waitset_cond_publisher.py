d5b78152c994c01f5971b8e37ec2af82bb936ace
Don Gochenour <don@rti.com>
2022-11-09 12:55:35 +0100

    initial from connext-py examples... with tweaks

---
+++ waitset_cond_publisher.py
@@ -0,0 +1,49 @@
+"""
+ (c) 2020 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ """
+
+import rti.connextdds as dds
+import time
+import argparse
+
+
+def publisher_main(domain_id, sample_count):
+    qos_provider = dds.QosProvider("./my_qos.xml")
+    participant = dds.DomainParticipant(domain_id, qos=qos_provider.participant_qos_from_profile("MyLibrary::MyProfile"))
+
+    wssc_type = dds.QosProvider("waitset_cond.xml").type("wssc_lib", "Foo")
+    topic = dds.DynamicData.Topic(participant, "Example Foo", wssc_type)
+    writer = dds.DynamicData.DataWriter(dds.Publisher(participant), topic, qos_provider.datawriter_qos_from_profile("MyLibrary::MyProfile"))
+
+    sample = dds.DynamicData(wssc_type)
+
+    count = 0
+    while (sample_count == 0) or (count < sample_count):
+        print("Writing Foo, count = {}".format(count))
+        sample["x"] = count
+        writer.write(sample)
+
+        count += 1
+        time.sleep(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="RTI Connext DDS Example: Waitsets with Status Condition & Read Condition (Publisher)"
+    )
+    parser.add_argument("-d", "--domain", type=int, default=0, help="DDS Domain ID")
+    parser.add_argument(
+        "-c", "--count", type=int, default=0, help="Number of samples to send"
+    )
+
+    args = parser.parse_args()
+    assert 0 <= args.domain < 233
+    assert args.count >= 0
+
+    publisher_main(args.domain, args.count)
---
+++ waitset_cond_subscriber.py
@@ -0,0 +1,84 @@
+"""
+ (c) 2020 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ """
+
+import rti.connextdds as dds
+import textwrap
+import time
+import argparse
+
+
+class Counter(object):
+    def __init__(self):
+        self.value = 0
+
+
+# Handler function for liveliness events
+def status_handler(reader):
+    status_mask = reader.status_changes
+    if dds.StatusMask.LIVELINESS_CHANGED in status_mask:
+        liveliness_status = reader.liveliness_changed_status
+        print(
+            "Liveliness changed => active writers = {}".format(
+                liveliness_status.alive_count
+            )
+        )
+
+# Handle incoming data with a ReadCondition
+def read_condition_handler(reader, count):
+    for (data, info) in filter(lambda s: s.info.valid, reader.take()):
+        count.value += 1
+        print(data)
+
+def subscriber_main(domain_id, sample_count):
+    qos_provider = dds.QosProvider("./my_qos.xml")
+    participant = dds.DomainParticipant(domain_id, qos=qos_provider.participant_qos_from_profile("MyLibrary::MyProfile"))
+
+    wsqc_type = dds.QosProvider("waitset_cond.xml").type("wssc_lib", "Foo")
+    topic = dds.DynamicData.Topic(participant, "Example Foo", wsqc_type)
+    reader = dds.DynamicData.DataReader(dds.Subscriber(participant), topic, qos_provider.datareader_qos_from_profile("MyLibrary::MyProfile"))
+#    reader = dds.DynamicData.DataReader(dds.Subscriber(participant), topic, reader_qos)
+
+    # Get the StatusCondition associated with the reader and set the mask to get liveliness updates
+    status_condition = dds.StatusCondition(reader)
+    status_condition.enabled_statuses = dds.StatusMask.LIVELINESS_CHANGED
+
+    # Create a ReadCondition to get any data
+    read_condition = dds.ReadCondition(reader, dds.DataState.any_data)
+
+    # Create WaitSet and attach conditions
+    waitset = dds.WaitSet()
+    waitset += status_condition
+    waitset += read_condition
+
+    count = Counter()
+    while (sample_count == 0) or (count.value < sample_count):
+        active = waitset.wait(4.0)
+        # Check conditions after wait to see if anything triggered
+        if status_condition in active:
+            status_handler(reader)
+        if read_condition in active:
+            read_condition_handler(reader, count)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="RTI Connext DDS Example: Waitsets with Status Condition & Read Condition (Subscriber)"
+    )
+    parser.add_argument("-d", "--domain", type=int, default=0, help="DDS Domain ID")
+    parser.add_argument(
+        "-c", "--count", type=int, default=0, help="Number of samples to send"
+    )
+
+    args = parser.parse_args()
+    assert 0 <= args.domain < 233
+    assert args.count >= 0
+
+    subscriber_main(args.domain, args.count)
